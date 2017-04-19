666c463c699a6a3b61f3274f480111145443ed18
Ram734 <prasannaking48@gmail.com>
2017-04-19 10:10:59 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class check
+{
+public static void main(String args[])
+{
+int a;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+if(a>0)
+{
+System.out.println("POSITIVE NUMBER");
+}
+else if(a==0)
+{
+System.out.print("ZERO");
+}
+else
+{
+System.out.print("NEGATIVE NUMBER");
+}
+}
+}
