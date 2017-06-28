4d3b05d71121c2c7667e3e3397c1699e209799b9
Ram734 <prasannaking48@gmail.com>
2017-06-28 22:49:47 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class count
+{
+	public static void main(String a[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String s=String.valueOf(n);
+		System.out.print(s.length());
+	}
+}
