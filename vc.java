95e9fe535aac2fda2fa5cdf81c96a480e6570c15
Ram734 <prasannaking48@gmail.com>
2017-06-28 22:29:14 +0530

    Add files via upload

---
+++ vc.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class vc
+{
+	public static void main(String a[])
+	{
+	Scanner sc=new Scanner(System.in);
+	char ch=sc.next().charAt(0);
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	{
+		System.out.print("Given character is vowel");
+	}
+	else
+	{
+		System.out.print("Given character is Consonant");
+	}
+	}
+}
+	
