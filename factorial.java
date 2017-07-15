7bc17d7f21976b4c7695d4f9714b9a09624a3b15
Ram734 <prasannaking48@gmail.com>
2017-07-15 12:17:07 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class factorial
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int c=sc.nextInt();
+int fact=1;
+if(c==0 || c==1)
+{
+System.out.print(1);
+}
+else
+{
+for(int i=2;i<=c;i++)
+{
+fact=fact*i;
+}
+System.out.print(fact);
+}
+}
+}
